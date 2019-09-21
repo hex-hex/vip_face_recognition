dafd6dd8ba120dc17783cb7bd8faac384d986137
Hugh <he.x.working@gmail.com>
2019-09-21 14:39:39 +1200

    fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
 import cv2
+import face_recognition
 
 
 def main():
@@ -7,6 +7,14 @@ def main():
     cap = cv2.VideoCapture(0)
     while (True):
         ret, frame = cap.read()
+        face_landmarks_list = face_recognition.face_landmarks(frame)
+        for face_landmarks in face_landmarks_list:
+            chin = face_landmarks['chin']
+            chin_line_list = zip(chin[0:-1], chin[1:])
+            for chin_line in chin_line_list:
+                cv2.line(frame, chin_line[0], chin_line[1], 2)
+            # face_landmarks['nose_bridge']
+            # face_landmarks['nose_tip']
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
